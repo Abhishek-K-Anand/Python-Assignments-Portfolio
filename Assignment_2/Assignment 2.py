598c54dffe2b90b73d5d2a8fc846f36ac1ed87cf
Abhishek Kumar Anand <125988635+Abhishek-K-Anand@users.noreply.github.com>
2025-05-13 01:31:49 +0530

    Control Structures in Python

---
+++ Assignment_2/Assignment 2.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number = "))
+
+if num % 2 == 0:
+    print("The number is even")
+
+else:
+    print(f'{num} is an odd number')
+
+print("==============================================================================================")
+sum  = 0
+for i in range(1,51):
+    sum = sum + i
+
+
+print(f'The sum of num from 1 to 50 is {sum}')
+
+
